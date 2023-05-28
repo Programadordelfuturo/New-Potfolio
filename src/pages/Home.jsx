8f6b45386aba5cd18fd4b2a527683551b2a4c246
ProgramadorDelFuturo <saraviatornero310@gmail.com>
2023-05-28 01:00:47 -0500

    corrección número dos de building

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import I from '../assets/photo.jpeg';
 import { useSelector } from 'react-redux';
 import changeLanguaje from "../utils/languaje";
-import Icons from './Icons.';
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiOutlineTwitter } from "react-icons/ai";
 import Layout from './Layout';
 import ContactMe from './ContactMe';
 import Bold from './Bold';
@@ -24,7 +26,11 @@ const Home = () => {
             </p>
             <Bold value={valueLanguaje} textSpanish={changeLanguaje[0].spanish} textEnglish={changeLanguaje[0].english } />
           </div>
-          <Icons/>
+          <div id='icons-social'>
+            <a href="https://github.com/Programadordelfuturo" target='_blank' ><AiFillGithub/></a>
+            <a href="https://www.linkedin.com/in/luis-enrique-saravia-tornero-1150b8253/" target='_blank' ><AiFillLinkedin/></a>
+            <a href="https://twitter.com/saravia_tornero" target='_blank' ><AiOutlineTwitter/></a>
+          </div>
         </div>
       </div>
       <ContactMe/>
